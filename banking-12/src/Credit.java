0d0ca0e6968af9404ed8623a3842d9e163015194
Nagappa <nagappapatharwat424@gmail.com>
2022-03-02 09:16:01 +0530

    creditcard system

---
+++ banking-12/src/Credit.java
@@ -0,0 +1,22 @@
+public class Credit {
+	public void getcreditscore() {
+		System.out.println("your credit score is  6");
+
+	}
+public void getcreditcardname(String str) {
+	System.out.println("credit card name "+str);
+}
+	public static void main(String[] args) {
+		System.out.println("this is credit card system");
+
+		Credit cr = new Credit();
+
+		cr.getcreditscore();
+		cr.getcreditcardname("mastercard");
+
+	}
+}
+
+class xyz{
+	
+}
